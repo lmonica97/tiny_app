02d2efeb6e565887825b60cda98dade43f0a1b06
Monica Liang <monicaliang@rocketmail.com>
2021-04-08 14:07:09 -0400

    Built server

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+
+app.get('/', (request, response) => response.send("This works!"))
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server is serving on port ${port}`))
