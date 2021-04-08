4957c60fb99d1408f741cbe77358eb0653f1d1ba
Monica Liang <monicaliang@rocketmail.com>
2021-04-08 14:47:12 -0400

    Built out question model

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const db = require('../backend/config/keys').mongoURI;
+
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Mongo's a gogo"))
+    .catch(err => console.log(err))
 
 app.get('/', (request, response) => response.send("This works!"))
 
